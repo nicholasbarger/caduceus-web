89fc744dae5e25845854ef3f31cde216d1c2cb3d
nicholasbarger <nicholas@nicholasbarger.com>
2017-09-22 09:01:34 -0500

    Close #1 - added fake work-order service.

--- src/app/work-orders/work-order.service.ts
+++ src/app/work-orders/work-order.service.ts
@@ -7,7 +7,12 @@ export class WorkOrderService {
 
   constructor() { }
 
-  getCollection(filter): Promise<WorkOrder[]> {
+  create(workOrder: WorkOrder): Promise<WorkOrder> {
+    workOrder.id = 1;
+    return Promise.resolve(workOrder);
+  }
+
+  getCollection(filter: any): Promise<WorkOrder[]> {
     // todo: add data from filter
     var fakeWorkOrders: WorkOrder[] = [
       new WorkOrder(1, new Date(), new Date(), 1000, 1, true, 100, 1, 1, 1),
@@ -18,5 +23,9 @@ export class WorkOrderService {
 
   getSingle(id: number): Promise<WorkOrder> {
     return Promise.resolve(new WorkOrder(1, new Date(), new Date(), 1000, 1, true, 100, 1, 1, 1));
+  }
+
+  update(workOrder: WorkOrder): Promise<WorkOrder> {
+    return Promise.resolve(workOrder);
   }
 }
